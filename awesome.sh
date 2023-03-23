e19b14a1dcf1a633c155de327b73ed9e4a8c0dd9
VeryGoodNaeme <127005284+VeryGoodNaeme@users.noreply.github.com>
2023-03-23 23:24:52 +0000

    Added Install Scripts

---
+++ awesome.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#install dependencies
+sudo pacman -S xorg lightdm lightdm-gtk-greeter awesome flameshot alacritty ranger fish rofi pipewire-audio wireplumber pipewire-pulse
+
+#clone dotfiles from github
+git clone https://github.com/VeryGoodNaeme/Dotfiles
+
+#copy dotfiles to the config directory
+cd dotfiles
+cp -r dotconfig ~/.config
+
---
+++ dwm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#install dependencies
+sudo pacman -S base-devel xorg lightdm lightdm-gtk-greeter flameshot alacritty ranger fish rofi pipewire-audio pipewire-pulse wireplumber
+
+#add dotfiles from github
+git clone https://github.com/VeryGoodNaeme/Dotfiles
+cd dotfiles
+cp -r dotconfig ~/.config
+cp -r dotdwm ~/.dwm
+sudo cp dwm.desktop /usr/share/xsessions/dwm.desktop
+cd ..
+
+#install dwm
+git clone https://github.com/VeryGoodNaeme/dwm-naeme
+cd dwm-naeme/gaps
+sudo make install
