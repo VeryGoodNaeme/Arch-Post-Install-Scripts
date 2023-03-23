041ae273953344ae532f5fe8ce84cf88b56de3b8
VeryGoodNaeme <127005284+VeryGoodNaeme@users.noreply.github.com>
2023-03-23 23:57:28 +0000

    Update awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #install dependencies
 sudo pacman -S xorg lightdm lightdm-gtk-greeter awesome flameshot alacritty ranger fish rofi pipewire-audio wireplumber pipewire-pulse
+sudo systemctl enable lightdm
 
 #clone dotfiles from github
 git clone https://github.com/VeryGoodNaeme/Dotfiles
 
 #copy dotfiles to the config directory
-cd dotfiles
+cd Dotfiles
 cp -r dotconfig ~/.config
 
